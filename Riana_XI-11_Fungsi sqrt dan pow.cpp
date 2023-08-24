b5f0a6b11845057babfe930b7ea8dfd912535e54
hera91r <141219744+hera91r@users.noreply.github.com>
2023-08-24 11:10:16 +0700

    Update Riana_XI-11_Fungsi sqrt dan pow.cpp

--- Riana_XI-11_Fungsi sqrt dan pow.cpp
+++ Riana_XI-11_Fungsi sqrt dan pow.cpp
@@ -1,40 +1,19 @@
 #include <cstdlib>
 #include <iostream>
+#include <cmath>
 using namespace std;
-
 int main()
 {
-//mendeklarasikan array dan variabel
-int A[2][2], B[2][2], C[2][2];
+   double x, Akar, Pangkat;
+   cout << "Masukkan Nilai x: ";
+   cin >> x;
 
-cout << "Masukkan Nilai A11 : ";
-cin>>A[0][0];
-cout << "Masukkan Nilai A12 : ";
-cin>>A[0][1];
-cout << "Masukkan Nilai A21 : ";
-cin>>A[1][0];
-cout << "Masukkan Nilai A22 : ";
-cin>>A[1][1];
+   Akar = sqrt(x);
+   Pangkat = pow(x, 2);
 
-cout << "Masukkan Nilai B11 : ";
-cin>>B[0][0];
-cout << "Masukkan Nilai B12 : ";
-cin>>B[0][1];
-cout << "Masukkan Nilai B21 : ";
-cin>>B[1][0];
-cout << "Masukkan Nilai A22 : ";
-cin>>B[1][1];
-
-//menghitung nilai array
-C[0][0] = (A[0][0] * B[0][0]) + (A[0][1] * B[1][0]);
-C[0][1] = (A[0][0] * B[0][1]) + (A[0][1] * B[1][1]);
-C[1][0] = (A[1][0] * B[0][0]) + (A[1][1] * B[1][0]);
-C[1][1] = (A[1][0] * B[0][1]) + (A[1][1] * B[1][1]);
-
-cout << "Nilai Matrix C adalah: " << endl;
-cout << " [ " <<C[0][0]<< " " <<C[0][1]<<" ]"<< endl;
-cout << " [ " <<C[1][0]<< " "<<C[0][1]<< " ]" << endl;
-
+cout << "Akar dari " << x << = " << Akar << endl;
+cout <<"Pangkat dari "<< x << " = " <<Pangkat<< endl;
+   
    system ("PAUSE");
    return 0;
 }
